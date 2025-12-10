b806372bd212cfca21db1507b51743063c768265
Mark Baiden <markbaiden17@gmail.com>
2025-12-10 14:59:00 +0000

    Fix file task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,14 @@
 USE alx_book_store;
 
-SHOW FULL COLUMNS FROM Books;
+SELECT
+    COLUMN_NAME,
+    COLUMN_TYPE
+
+FROM
+    INFORMATION_SCHEMA.COLUMNS
+
+WHERE
+    TABLE_SCHEMA = 'alx_book_store'
+
+AND
+    TABLE_NAME = 'Books';
